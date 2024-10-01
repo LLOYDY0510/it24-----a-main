4e0bc61f41d769e4e7b49760167f51083702492b
LLOYDY05 <johnlloyddonque18@gmail.com>
2024-10-01 08:46:49 +0800

    Added Arithmetic and Logical Operators

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ let number = 7;
 let isEven = false;
 let primeStatus = ""; 
 let palindromeStatus = "";
+// 2. Arithmetic and Logical Operators
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b; 
